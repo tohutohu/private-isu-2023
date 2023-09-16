a2203de390ae4c1f156481f55015e11e74d1813f
tohutohu <tohu.soy@gmail.com>
2023-09-16 23:35:33 +0900

    unix domain socketでnginxにつなぐ

--- golang/app.go
+++ golang/app.go
@@ -14,6 +14,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -1000,5 +1001,29 @@ func main() {
 	// add pprof
 	r.Mount("/debug", middleware.Profiler())
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	socketFile := "/var/run/isu-go/app.sock"
+	os.Remove(socketFile)
+
+	l, err := net.Listen("unix", socketFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// go runユーザとnginxのユーザ（グループ）を同じにすれば777じゃなくてok
+	err = os.Chmod(socketFile, 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	defer l.Close()
+
+	// Unixドメインソケット上でHTTPサーバーを起動
+	httpServer := &http.Server{
+		Handler: r,
+	}
+
+	if err := httpServer.Serve(l); err != nil {
+		log.Fatal("Failed to serve:", err)
+	}
+	//log.Fatal(http.ListenAndServe(":8080", r))
 }
